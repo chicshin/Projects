d49759c8d244b1d336c22051eee7f5d680e9f083
chicshin <inchicshin@gmail.com>
2019-04-18 02:11:01 -0400

    add tags in comments

--- crawling/ig_top_posts_tags.py
+++ crawling/ig_top_posts_tags.py
@@ -1,8 +1,7 @@
 """
-Scrap Instagram javascript url.
-Get the top posts' hashtags.
-Save it into 'ig_top_posts_tags2.txt'
-The saved data will be analyzed for in use of tags recommendation.
+Scrap Instagram javascript url by using shortcode.
+Get the hashtags from captions and comments.
+Saved data will be analyzed for in use of tags recommendation.
 """
 import json
 import urllib.request
@@ -10,25 +9,43 @@ import urllib.request
 tag = 'yoga'
 url = 'https://www.instagram.com/explore/tags/' + tag + '/?__a=1'
 
-with urllib.request.urlopen(url) as response:
-    source = response.read()
+def get_json(url):
+    with urllib.request.urlopen(url) as response:
+        source = response.read()
+    data = json.loads(source.strip(), strict=False)
+    # with open('tag_json.py', 'w') as f:
+        # json.dump(data, f, indent=4)
+    return data
 
-# dump javascript data for easily readable
-data = json.loads(source.strip(), strict=False)
-with open("soup.py", "w") as f:
-    json.dump(data, f, indent=4)
+data = get_json(url)
+path = data['graphql']['hashtag']['edge_hashtag_to_top_posts']['edges']
 
-def get_tags(data):
-    tags_list = []
-    for post in data['graphql']['hashtag']['edge_hashtag_to_top_posts']['edges']:
-        caption = post['node']['edge_media_to_caption']['edges'][0]['node']['text'].split(" ")
-        for text in caption:
-            if text.strip().startswith('#') and text.strip() not in tags_list:
-                tags_list.append(text.strip())
-    return tags_list
-get_tags(data)
+def get_shortcode():
+    shortcode_urls = []
+    for post in path:
+        shortcode = post['node']['shortcode']
+        url = 'http://www.instagram.com/p/' + shortcode + '/?__a=1'
+        shortcode_urls.append(url)
+    return shortcode_urls
+# print(get_shortcode(path))
 
-# write the data in external file
-with open('ig_top_posts_tags2.txt', 'w') as f:
-    for line in get_tags(data):
-        f.write('%s\n' % line)
+def get_tags():
+    tag_list =[]
+    url = get_shortcode()
+    for i in range(len(url)):
+        with urllib.request.urlopen(url[i]) as response:
+            source = response.read()
+        data = json.loads(source.strip(), strict=False)
+
+        for post in data['graphql']['shortcode_media']['edge_media_to_caption']['edges']:
+            caption_tags = post['node']['text'].split(' ')
+            for text in caption_tags:
+                if text.strip().startswith('#') and text.strip() not in tag_list:
+                    tag_list.append(text.strip())
+        for post in data['graphql']['shortcode_media']['edge_media_to_comment']['edges']:
+            comment_tags = post['node']['text'].split(' ')
+            for text in comment_tags:
+                if text.strip().startswith('#') and text.strip() not in tag_list:
+                    tag_list.append(text.strip())
+    print(tag_list)
+get_tags()
